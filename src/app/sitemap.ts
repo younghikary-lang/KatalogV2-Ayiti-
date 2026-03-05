0d94ea46b0d64cc94d69a669c20b18922b41667a
Stuart Blanc <young.hikary@gmail.com>
2026-03-05 04:01:59 -0500

    fix sitemap dynamic rendering

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://katalog.ht';
 
